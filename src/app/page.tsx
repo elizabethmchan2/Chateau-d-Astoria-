e327e73f2951223ecf5a971715f62e2dafda9bc2
elizabethmchan2 <elizabethmchan2@gmail.com>
2024-04-26 18:58:28 -0700

    update for linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1> CHATEAU D'ASTORIA </h1>
-        <img src='chateau.jpg'></img>
+      <div className="grid grid-cols-2 gap-10 col-auto justify-center items-center">
+        <h1> CHATEAU D&lsquo;ASTORIA </h1>
+        <Image src='/chateau.jpg' alt="chateau d'astoria logo" width={500} height={500}></Image>
       </div>
     </main>
   );
