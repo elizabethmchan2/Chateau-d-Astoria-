5b309b426ac70b7da10aff225fcaa0aa64ca7dd9
elizabethmchan2 <elizabethmchan2@gmail.com>
2024-04-26 19:34:09 -0700

    update config and image location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="grid grid-cols-2 gap-10 col-auto justify-center items-center">
         <h1> CHATEAU D&lsquo;ASTORIA </h1>
-        <Image src='/chateau.jpg' alt="chateau d'astoria logo" width={500} height={500}></Image>
+        <Image src='/Chateau.jpg' alt="chateau d'astoria logo" width={500} height={500}></Image>
       </div>
     </main>
   );
